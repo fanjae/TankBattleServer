6dbb4ad13fb71df1fbc62c9b9c998a0bb5d4809b
FanJae <hjjang45@gmail.com>
2026-05-24 21:39:19 +0900

    feat : 대포에 대한 상태 정보를 가지는 DTO 추가
    대표에 대한 상태 정보를 가지는 DTO를 추가하였습니다.

---
+++ TankBattleServer/Packets/CannonBallState.cs
@@ -0,0 +1,9 @@
+﻿public class CannonBallState
+{
+    public int CannonBallId { get; set; }
+    public int OwnerPlayerId { get; set; }
+
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+}
