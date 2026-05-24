f9223823037379df29d0f757b169a3808c3ab76d
FanJae <hjjang45@gmail.com>
2026-05-24 14:58:00 +0900

    feat : 플레이어 접속 확인에 대한 확인 패킷 추가
    Player가 접속시 Welcome Packet을 Client 측으로 보내서 확인하도록 처리

---
+++ TankBattleServer/Packets/WelcomePacket.cs
@@ -0,0 +1,7 @@
+﻿namespace TankBattleServer.Packets;
+public class WelcomePacket
+{
+    public string Type { get; set; } = "Welcome";
+    public int PlayerId { get; set; }
+    public string Message { get; set; } = "";
+}
