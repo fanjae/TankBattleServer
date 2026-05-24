608854a7ac432adb5458e5f1c5c19f1794030bac
FanJae <hjjang45@gmail.com>
2026-05-24 21:38:51 +0900

    feat : 대포 상태 패킷을 보내기 위한 데이터 추가
    대포에 대한 상태 값을 보내기 위한 데이터를 추가하였습니다.

--- TankBattleServer/Packets/StatePacket.cs
+++ TankBattleServer/Packets/StatePacket.cs
@@ -1,5 +1,8 @@
 ﻿public class StatePacket
 {
     public string Type { get; set; } = "State";
+
     public TankState[] Tanks { get; set; } = Array.Empty<TankState>();
+
+    public CannonBallState[] CannonBalls { get; set; } = Array.Empty<CannonBallState>();
 }
