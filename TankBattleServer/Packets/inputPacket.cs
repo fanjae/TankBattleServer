1a5932b6eb93cb8db6c0afffe4e8eb535d9ff876
FanJae <hjjang45@gmail.com>
2026-05-24 14:58:42 +0900

    feat : 플레이어 입력과 관련된 패킷 정보

---
+++ TankBattleServer/Packets/inputPacket.cs
@@ -0,0 +1,12 @@
+﻿public class InputPacket // 플레이어 입력 패킷
+{
+    public string Type { get; set; } = "Input";
+    public int PlayerId { get; set; }
+
+    public float Move { get; set; }
+    public float Turn { get; set; }
+    public float Turret { get; set; }
+    public float Gun { get; set; }
+
+    public bool Fire { get; set; }
+}
