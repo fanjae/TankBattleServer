70cb19cd1e90d43272b6c3af326faf2f9570dcea
FanJae <hjjang45@gmail.com>
2026-05-24 15:02:49 +0900

    feat : 서버 기반 탱크 상태 정보 업데이트 추가
    기존 Unity에서 다루던 탱크에 대한 정보를 서버에서 다룰 수있도록 변경하였습니다.

---
+++ TankBattleServer/ServerTank.cs
@@ -0,0 +1,58 @@
+﻿namespace TankBattleServer;
+
+public class ServerTank
+{
+    public int PlayerId;
+
+    public float x;
+    public float z;
+    public float angle;
+
+    public float turretTurn;
+    public float gunPitch;
+
+    public int Hp = 100;
+
+    // 클라이언트에게 받은 입력값
+    public InputPacket LastInput = new();
+
+    private const float TankSpeed = 5f;
+    private const float ReverseSpeed = 3f;
+    private const float TurnSpeed = 120f;
+    private const float TurretTurnSpeed = 120f;
+    private const float GunPitchSpeed = 30f;
+
+    public void Update(float deltaTime)
+    {
+        UpdateBody(deltaTime);
+        UpdateTurret(deltaTime);
+        UpdateGun(deltaTime);
+    }
+
+    private void UpdateBody(float deltaTime) // 탱크 몸체 이동 및 회전 
+    {
+        float speed = LastInput.Move >= 0f ? TankSpeed : ReverseSpeed; // 전진 속도와 후진 속도 처리
+
+        angle += LastInput.Turn * TurnSpeed * deltaTime;
+
+        // 도 단위에서 라디안 단위로 변환
+        float rad = angle * MathF.PI / 180f;
+
+        // X축 이동 비율, Z축 이동 비율을 각각 구하기 위해 Sin, Cos으로 구함.
+        x += MathF.Sin(rad) * LastInput.Move * speed * deltaTime;
+        z += MathF.Cos(rad) * LastInput.Move * speed * deltaTime;
+    }
+
+    private void UpdateTurret(float deltaTime) // 터렛 좌우 회전
+    {
+        turretTurn += LastInput.Turret * TurretTurnSpeed * deltaTime;
+    }
+
+    private void UpdateGun(float deltaTime) // 포신 상하 회전
+    {
+        gunPitch += LastInput.Gun * GunPitchSpeed * deltaTime;
+        gunPitch = Math.Clamp(gunPitch, -10f, 30f);
+
+
+    }
+}
